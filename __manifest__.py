6c6f600bcd8aee42e44f34cd2db8869931a92adb
raguzman <55261233+rafaguzmang@users.noreply.github.com>
2024-05-24 12:06:39 -0600

    first commit

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name":"Minutas",
+
+    'version': '1.0',
+    'author': "Rafael Guzmán",
+    "description": "Llevar el control de las juntas de producción",
+    "data":[
+        'security/ir.model.access.csv',
+        'views/dtm_minutas_view.xml'
+    ],
+    'installable': True,
+    'application': True,
+}
+
---
+++ models/dtm_minutas.py
@@ -0,0 +1,30 @@
+from odoo import api,models,fields
+from datetime import datetime
+
+class Minutas(models.Model):
+    _name = "dtm.minutas"
+    _description = "Modelo para llevar el control de la juntas de Procesos"
+
+    fecha = fields.Date(string="Fecha",default = datetime.today())
+    anataciones = fields.Text(string="Anotaciones")
+    asistentes = fields.One2many("dtm.minutas.asistentes","model_id")
+
+
+class Asistentes(models.Model):
+    _name = "dtm.minutas.asistentes"
+    _description = "Modelo para llevar la lista de asistentes"
+
+    model_id = fields.Many2one("dtm.minutas")
+    asistente = fields.Many2one("dtm.minutas.nombres",string="Nombre")
+    actividades = fields.Text(string="Actividades")
+
+
+class Nombres(models.Model):
+    _name = "dtm.minutas.nombres"
+    _description = "Modelo para almacenar los nombres de los participantes"
+    _rec_name = "nombre"
+
+    nombre = fields.Char(string="Participante")
+
+
+
