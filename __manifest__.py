2fb258df472b7339d07b77b3693c21fb3743717e
rafaguzmang <55261233+rafaguzmang@users.noreply.github.com>
2026-08-11 12:46:13 -0600

    Se actualiza menú

--- __manifest__.py
+++ __manifest__.py
@@ -5,6 +5,7 @@
     'author': "Rafael Guzmán",
     "description": "Llevar el control de las juntas de producción",
     "data":[
+        'security/res_group.xml',
         'security/ir.model.access.csv',
         'views/dtm_minutas_view.xml'
     ],
